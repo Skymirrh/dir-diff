d70ad260410c9ccd1252d87307f0465423f566dd
Skymirrh <skymirrh@skymirrh.net>
2016-03-15 00:45:59 +0100

    Fix build script

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
 :: Simple build script
 :: Pyinstaller must be installed: python -m pip install pyinstaller
 :: The Python Scripts folder must be in your %PATH%
-pyinstaller --clean --version-file=VERSION --onefile alot_diff.py
+pyinstaller --clean --version-file=VERSION --onefile dir_diff.py
